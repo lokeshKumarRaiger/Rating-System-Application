f150678469f4eed4b18476e2e85b912bfde7b8e6
Lokesh kumar Raiger <168194176+lokeshKumarRaiger@users.noreply.github.com>
2025-06-17 20:13:30 +0530

    Delete rating-system directory

--- rating-system/src/main/java/com/rating_system/rating_system/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rating_system.rating_system.repository;
-
-import com.rating_system.rating_system.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // No extra methods needed for now
-}
--- rating-system/src/main/java/com/rating_system/rating_system/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rating_system.rating_system.repository;
-
-import com.rating_system.rating_system.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    boolean existsByUsernameAndProductId(String username, Long productId);
-}
